29becc2c5cf6ff769607e5b447526fc7b98df837
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-04 19:51:41 +0900

    feat: Update theme.lua with shaunsingh/nord.nvim colorscheme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,8 +1,19 @@
 return {
-    "lalitmee/cobalt2.nvim",
-    event = { "ColorSchemePre" }, -- if you want to lazy load
-    dependencies = { "tjdevries/colorbuddy.nvim" },
-    init = function()
-        require("colorbuddy").colorscheme("cobalt2")
-    end
+    'shaunsingh/nord.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd[[colorscheme nord]]
+
+      -- Example config in lua
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = false
+      vim.g.nord_disable_background = true
+      vim.g.nord_italic = false
+      vim.g.nord_uniform_diff_background = true
+      vim.g.nord_bold = false
+
+      -- Load the colorscheme
+      require('nord').set()
+    end,
 }
