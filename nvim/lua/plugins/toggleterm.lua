962eaed25395f70c06745e7bbd1c13357fe950b0
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-08 21:34:31 +0900

    feat: Add coc.nvim and toggleterm.nvim plugins

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      -- ToggleTerm のカスタム設定
+      require("toggleterm").setup {
+        size = 20,
+        open_mapping = [[<c-\>]],
+        direction = 'float',
+      }
+
+      -- FIXME: make terminal effective
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_set_keymap("n", "<leader>j", ":ToggleTerm<CR>", opts)
+      vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=horizontal name=desktop<CR>", opts)
+    end
+  }
+}
