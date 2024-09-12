1612a9e383bdb4480a970d43034f8bbc0adbc2eb
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-12 16:03:43 +0900

    feat: Update open_mapping keybinding

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -6,7 +6,7 @@ return {
       -- ToggleTerm のカスタム設定
       require("toggleterm").setup {
         size = 20,
-        open_mapping = [[<c-\>]],
+        open_mapping = [[<c-b>]],
         direction = 'float',
       }
 
