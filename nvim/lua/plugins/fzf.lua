af4c8a2ad575781734897a1dcaaffab1aff5f762
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-13 23:10:17 +0900

    feat: Add keymaps in fzf-lua and format file

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -4,24 +4,26 @@ return {
     config = function()
         require("fzf-lua").setup({
             files = {
-                -- Changed to POSIX-compliant options for 'find' 
+                -- Changed to POSIX-compliant options for 'find'
                 find_opts = [[-type f]]
             },
             keymap = {
                 fzf = {
                     ["tab"] = "down",
+                    ["ctrl-j"] = "down",
                     ["shift-tab"] = "up",
+                    ["ctrl-k"] = "up",
                 },
             }
         })
 
         local opts = { noremap = true, silent = true }
 
-        -- File name search 
+        -- File name search
         vim.api.nvim_set_keymap("n", "<leader>p", ":FzfLua files<CR>", opts)
         -- Search within all file
         vim.api.nvim_set_keymap("n", "<leader>g", ":FzfLua live_grep<CR>", opts)
-        -- Search within the current file 
+        -- Search within the current file
         vim.api.nvim_set_keymap("n", "<leader>f", ":FzfLua lines<CR>", opts)
     end
 }
