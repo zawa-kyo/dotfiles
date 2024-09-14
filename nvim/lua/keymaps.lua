7a858529618d8cd2d2488c2a560154e7a0f02f86
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-14 23:26:26 +0900

    refactor: Refactor keymaps to use newer API method

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,7 +2,7 @@ local opts = { noremap = true, silent = true }
 local term_opts = { silent = true }
 
 --local keymap = vim.keymap
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
 --Remap space as leader key
 keymap("", "<Space>", "<Nop>", opts)
