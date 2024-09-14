6999dd80f88e025293afd0dda9539e612b85cd4c
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-09-14 23:39:45 +0900

    fix: Resolve incorrect behavior when using mouse in terminal mode

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -9,9 +9,25 @@ return {
         direction = 'float',
       }
 
-      -- FIXME: make terminal effective
+      -- Key mapping to open the terminal
       local opts = { noremap = true, silent = true }
       vim.api.nvim_set_keymap("n", "<leader>t", ":ToggleTerm direction=horizontal name=desktop<CR>", opts)
-    end
+
+      -- Disable mouse when entering terminal mode
+      vim.api.nvim_create_autocmd("TermEnter", {
+        pattern = "*",
+        callback = function()
+          vim.opt.mouse = ""
+        end,
+      })
+
+      -- Re-enable mouse when leaving terminal mode
+      vim.api.nvim_create_autocmd("TermLeave", {
+        pattern = "*",
+        callback = function()
+          vim.opt.mouse = "a"
+        end,
+      })
+    end,
   }
 }
