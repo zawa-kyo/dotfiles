0a5f106630ac3b17e9d1abc725e822ba973c9649
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-12-12 19:33:01 +0900

    fix: Update path variable to use HOME instead of ZDOTDIR

--- scripts/source-zshrc.sh
+++ scripts/source-zshrc.sh
@@ -1,9 +1,9 @@
 # Check if the local configuration directory exists
-if [ -d "$ZDOTDIR/local.d" ]; then
-  for conf_file in "$ZDOTDIR/local.d/"*.zsh; do
+if [ -d "$HOME/local.d" ]; then
+  for conf_file in "$HOME/local.d/"*.zsh; do
     # If no .zsh files are found, exit the loop
     [ -e "$conf_file" ] || {
-      echo "🚧 No configuration files found in $ZDOTDIR/local.d"
+      echo "🚧 No configuration files found in $HOME/local.d"
       break
     }
     source "${conf_file}"
@@ -13,5 +13,5 @@ if [ -d "$ZDOTDIR/local.d" ]; then
   done
 else
   # If the configuration directory does not exist
-  echo "❌ Configuration directory not found: $ZDOTDIR/local.d"
+  echo "❌ Configuration directory not found: $HOME/local.d"
 fi
