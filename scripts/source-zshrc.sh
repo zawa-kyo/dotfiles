37e305100f857bd081948bcd88a28d63f3eba0d5
Kyohei Ozawa <zawazawakyo@gmail.com>
2024-12-12 22:17:08 +0900

    refactor: Refactor sourcing logic to function with parameters

--- scripts/source-zshrc.sh
+++ scripts/source-zshrc.sh
@@ -1,17 +1,21 @@
-# Check if the local configuration directory exists
-if [ -d "$HOME/local.d" ]; then
-  for conf_file in "$HOME/local.d/"*.zsh; do
-    # If no .zsh files are found, exit the loop
-    [ -e "$conf_file" ] || {
-      echo "🚧 No configuration files found in $HOME/local.d"
-      break
-    }
-    source "${conf_file}"
+source_files_in_dir() {
+  local config_dir="$1"
+  local extension="$2"
 
-    # Confirm the file was sourced
-    echo "✅ Successfully sourced: $conf_file"
-  done
-else
-  # If the configuration directory does not exist
-  echo "❌ Configuration directory not found: $HOME/local.d"
-fi
+  if [ -d "$config_dir" ]; then
+    for conf_file in "$config_dir/"*"$extension"; do
+      # If no files with the given extension are found, print a message and break
+      [ -e "$conf_file" ] || {
+        echo "🚧 No configuration files with extension $extension found in $config_dir"
+        break
+      }
+      source "${conf_file}"
+      echo "✅ Successfully sourced: $conf_file"
+    done
+  else
+    # If the configuration directory does not exist
+    echo "❌ Configuration directory not found: $config_dir"
+  fi
+}
+
+source_files_in_dir "$HOME/local.d" ".zsh"
