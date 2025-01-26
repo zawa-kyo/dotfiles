12e33cbd657c74043702fef31ebcc3a9eb6c5c61
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-01-26 16:20:45 +0900

    style: Align indentation

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -71,16 +71,16 @@ return {
 
             -- Reference highlight
             vim.cmd [[
-        set updatetime=500
-        highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-        highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-        highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-        augroup lsp_document_highlight
-          autocmd!
-          autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-          autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-        augroup END
-      ]]
+                set updatetime=500
+                highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
+                highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
+                highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
+                augroup lsp_document_highlight
+                autocmd!
+                autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+                augroup END
+            ]]
         end,
     },
 
