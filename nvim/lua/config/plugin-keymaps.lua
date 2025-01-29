f0427243fcfd3eb5255d8c8919626a79bb5bb2fc
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-01-29 11:22:34 +0900

    refactor: Separate Neoscroll keymaps into a dedicated file

--- nvim/lua/config/plugin-keymaps.lua
+++ nvim/lua/config/plugin-keymaps.lua
@@ -123,6 +123,27 @@ vim.keymap.set("n", "<leader>f", fzf.lines, opts)
 
 
 --------------------
+-- Neoscroll
+--------------------
+
+-- Load the plugin
+local neoscroll = require("neoscroll")
+
+local keymap = {
+    ["<C-u>"]   = function() neoscroll.ctrl_u({ duration = 250 }) end,
+    ["<C-d>"]   = function() neoscroll.ctrl_d({ duration = 250 }) end,
+    ["<C-b>"]   = function() neoscroll.ctrl_b({ duration = 550 }) end,
+    ["<C-f>"]   = function() neoscroll.ctrl_f({ duration = 550 }) end,
+    ["<Tab>"]   = function() neoscroll.ctrl_d({ duration = 550 }) end,
+    ["<S-Tab>"] = function() neoscroll.ctrl_u({ duration = 550 }) end,
+}
+
+for key, func in pairs(keymap) do
+    vim.keymap.set({ "n", "v", "x" }, key, func, { silent = true })
+end
+
+
+--------------------
 -- Dial
 --------------------
 
--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -1,21 +1,9 @@
-return {
+local M = {
     "karb94/neoscroll.nvim",
+    lazy = false,
     config = function()
-        local neoscroll = require("neoscroll")
-        neoscroll.setup({ easing = "sine" })
-
-        local keymap = {
-            ["<C-u>"]   = function() neoscroll.ctrl_u({ duration = 250 }) end,
-            ["<C-d>"]   = function() neoscroll.ctrl_d({ duration = 250 }) end,
-            ["<C-b>"]   = function() neoscroll.ctrl_b({ duration = 550 }) end,
-            ["<C-f>"]   = function() neoscroll.ctrl_f({ duration = 550 }) end,
-            ["<Tab>"]   = function() neoscroll.ctrl_d({ duration = 550 }) end,
-            ["<S-Tab>"] = function() neoscroll.ctrl_u({ duration = 550 }) end,
-        }
-
-        local modes = { "n", "v", "x" }
-        for key, func in pairs(keymap) do
-            vim.keymap.set(modes, key, func, { silent = true })
-        end
+        require("neoscroll").setup({ easing = "sine" })
     end,
 }
+
+return M
