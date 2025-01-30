b6f0d8f79aad6a8100079941e11d4c16c17c54e0
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-01-30 14:30:37 +0900

    feat: Extend opts function

--- nvim/lua/config/utils.lua
+++ nvim/lua/config/utils.lua
@@ -1,16 +1,49 @@
 -- Module for utility functions
 M = {}
 
+--- Check if a value is not nil
+--- @param value any The value to check
+--- @return boolean Whether the value is not nil
+function M.isNotNil(value)
+    return value ~= nil
+end
+
+--- Check if a value is a string
+--- @param value any The value to check
+--- @return boolean Whether the value is not nil
+function M.isString(value)
+    return type(value) == "string"
+end
+
+--- Check if a value is a boolean
+--- @param value any The value to check
+--- @return boolean Whether the value is a boolean
+function M.isBoolean(value)
+    return type(value) == "boolean"
+end
+
 --- Get options with a description
 --- @param desc string|nil Description of the mapping
+--- @param is_noremap boolean|nil Whether the mapping should be noremap (default: true)
+--- @param is_silent boolean|nil Whether the mapping should be silent (default: true)
 --- @return table opts A table containing keymap options
-function M.getOpts(desc)
-    -- Clone opts() to avoid modifying the original table
+function M.getOpts(desc, is_noremap, is_silent)
+    -- Default options
     local options = { noremap = true, silent = true }
 
-    if desc then
+    if M.isBoolean(is_noremap) then
+        options.noremap = is_noremap
+    end
+
+    if M.isBoolean(is_silent) then
+        options.silent = is_silent
+    end
+
+    if M.isString(desc) then
         options.desc = desc
     end
+
+
     return options
 end
 
