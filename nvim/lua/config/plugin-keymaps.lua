a06bea6669e79705668b9f5ca558f76e307b220a
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-01-30 10:48:50 +0900

    feat: Refactor LSP keymaps and add descriptions

--- nvim/lua/config/plugin-keymaps.lua
+++ nvim/lua/config/plugin-keymaps.lua
@@ -72,18 +72,54 @@ vim.keymap.set("n", "<Esc>", close_window, opts())
 -- LSP
 --------------------
 
-vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover()<CR>")
-vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.format()<CR>")
-vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
-vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-vim.keymap.set("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-vim.keymap.set("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
-vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+vim.keymap.set(
+    'n', 'K', vim.lsp.buf.hover,
+    opts("Show hover information")
+)
+vim.keymap.set(
+    "n", "gf", vim.lsp.buf.format,
+    opts("Format the current file")
+)
+vim.keymap.set(
+    "n", "gr", vim.lsp.buf.references,
+    opts("Show references")
+)
+vim.keymap.set(
+    "n", "gd", vim.lsp.buf.definition,
+    opts("Go to definition")
+)
+vim.keymap.set(
+    "n", "gD", vim.lsp.buf.declaration,
+    opts("Go to declaration")
+)
+vim.keymap.set(
+    "n", "gi", vim.lsp.buf.implementation,
+    opts("Go to implementation")
+)
+vim.keymap.set(
+    "n", "gt", vim.lsp.buf.type_definition,
+    opts("Go to type definition")
+)
+vim.keymap.set(
+    "n", "rn", vim.lsp.buf.rename,
+    opts("Rename the symbol under cursor")
+)
+vim.keymap.set(
+    "n", "ga", vim.lsp.buf.code_action,
+    opts("Show available code actions")
+)
+vim.keymap.set(
+    "n", "ge", vim.diagnostic.open_float,
+    opts("Show diagnostics")
+)
+vim.keymap.set(
+    "n", "g]", vim.diagnostic.goto_next,
+    opts("Go to next diagnostic issue")
+)
+vim.keymap.set(
+    "n", "g[", vim.diagnostic.goto_prev,
+    opts("Go to previous diagnostic issue")
+)
 
 
 --------------------
