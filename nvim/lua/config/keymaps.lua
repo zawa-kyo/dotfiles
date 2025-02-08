ef2714ef09bf0e70a6c900ba8ac55134d584b0a2
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-02-08 22:53:02 +0900

    feat: Add new window resizing keymaps and equalizing sizes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -78,8 +78,18 @@ keymap("n", "zk", "zb", opts("Look up"))
 keymap("n", "zj", "zt", opts("Look down"))
 
 -- Split window
-keymap("n", "split", ":split<Return><C-w>w", opts("Split window horizontally"))
-keymap("n", "splitv", ":vsplit<Return><C-w>w", opts("Split window vertically"))
+keymap("n", "s", "<Nop>", opts("Nop"))
+keymap("n", "ss", ":split<Return><C-w>w", opts("Split window horizontally"))
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts("Split window vertically"))
+
+-- Resize window
+keymap("n", "sh", "5<C-w><", opts("Decrease window width"))
+keymap("n", "sl", "5<C-w>>", opts("Increase window width"))
+keymap("n", "sj", "5<C-w>-", opts("Decrease window height"))
+keymap("n", "sk", "5<C-w>+", opts("Increase window height"))
+
+-- Equalize window sizes
+keymap("n", "se", "<C-w>=", opts("Equalize window sizes"))
 
 -- Do not yank with x
 keymap("n", "x", '"_x', opts("Do not yank with x"))
