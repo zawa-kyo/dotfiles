2b5b594d9a228bdc79e0c27a4e0f9d0f3218e212
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-02-08 13:16:17 +0900

    feat: Update nvim-treesitter configuration

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,14 +1,40 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "lua",
-            "typescript",
-            "javascript"
-        },
-        sync_install = true,
-        indent = {
-            enable = true
+    'nvim-treesitter/nvim-treesitter',
+    event = { 'BufNewFile', 'BufRead' },
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        'nvim-treesitter/nvim-treesitter-context',
+    },
+    build = ":TSUpdate",
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            ensure_installed = {
+                "lua",
+                "javascript",
+                "typescript",
+                "python",
+            },
+            highlight = {
+                enable = true,
+            },
+            autotag = {
+                enable = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = {
+                            query = "@class.inner",
+                            desc = "Select inner part of a class region",
+                        },
+                    }
+                },
+            }
         }
-    }
+    end
 }
