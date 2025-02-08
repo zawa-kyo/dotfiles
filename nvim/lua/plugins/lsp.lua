a9a85a47748bfef52bb622a11402dd06f22acaed
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-02-08 13:56:30 +0900

    fix: Method textDocument/documentHighlight is not supported
    Warning message: method textDocument/documentHighlight is not supported by any of the servers registered for the current buffer

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -89,19 +89,6 @@ table.insert(M, {
         vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
             vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
         )
-
-        -- Reference highlight
-        vim.cmd [[
-            set updatetime=500
-            highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-            highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-            highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#F1943B guibg=#374152
-            augroup lsp_document_highlight
-            autocmd!
-            autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-            autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-            augroup END
-        ]]
     end,
 })
 
