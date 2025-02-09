444bd0f8fcedb9a7d73dd62063c79fe904152d44
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-02-09 21:28:45 +0900

    feat: Reconfigure displayed items in lualine and refactor code for better readability

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,13 +1,32 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require('lualine').setup({
+        require("lualine").setup({
             options = {
-                theme = 'auto',
-                section_separators = '',
-                component_separators = ''
-            }
+                theme = "auto",
+                icons_enabled = true,
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+            },
+
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_c = { "filename" },
+                lualine_x = { "encoding", "filetype" },
+                lualine_y = { "progress" },
+                lualine_z = { "location" }
+            },
+
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {}
+            },
         })
     end
 }
