3d4ee414ac397913e205abede62877e081ff64e8
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-08-09 17:06:07 +0900

    fix: Guard setup_handlers usage and add TOML server

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ M.servers = {
   "lua_ls", -- Lua
   "ts_ls", -- TypeScript
   "pyright", -- Python
+  "taplo", -- TOML
 }
 
 -- Neoconf (should run before LSP configuration)
@@ -109,37 +110,53 @@ table.insert(M, {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "williamboman/mason.nvim",
   },
 
   config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = M.servers,
-    })
-    require("mason-lspconfig").setup_handlers({
-      function(server)
-        local opt = {
-          capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        }
-        opt.on_attach = function(_, bufnr)
-          local function map(mode, lhs, rhs, desc)
-            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, noremap = true, desc = desc })
-          end
-          map("n", "K", vim.lsp.buf.hover, "Show hover information")
-          map("n", "gf", vim.lsp.buf.format, "Format the current file")
-          map("n", "gr", vim.lsp.buf.references, "Show references")
-          map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-          map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-          map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
-          map("n", "rn", vim.lsp.buf.rename, "Rename the symbol")
-          map("n", "ga", vim.lsp.buf.code_action, "Show available code actions")
-          map("n", "ge", vim.diagnostic.open_float, "Show diagnostics")
-          map("n", "g]", vim.diagnostic.goto_next, "Go to next diagnostic issue")
-          map("n", "g[", vim.diagnostic.goto_prev, "Go to previous diagnostic issue")
+    local mason_lspconfig = require("mason-lspconfig")
+    mason_lspconfig.setup({ ensure_installed = M.servers })
+
+    local function setup_server(server)
+      local lsp = require("lspconfig")
+      if not lsp[server] then
+        return -- Skip unknown servers on older lspconfig versions
+      end
+
+      local opt = { capabilities = require("cmp_nvim_lsp").default_capabilities() }
+      opt.on_attach = function(_, bufnr)
+        local function map(mode, lhs, rhs, desc)
+          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, noremap = true, desc = desc })
         end
-        require("lspconfig")[server].setup(opt)
-      end,
-    })
+        map("n", "K", vim.lsp.buf.hover, "Show hover information")
+        map("n", "gf", vim.lsp.buf.format, "Format the current file")
+        map("n", "gr", vim.lsp.buf.references, "Show references")
+        map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+        map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+        map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+        map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
+        map("n", "rn", vim.lsp.buf.rename, "Rename the symbol")
+        map("n", "ga", vim.lsp.buf.code_action, "Show available code actions")
+        map("n", "ge", vim.diagnostic.open_float, "Show diagnostics")
+        map("n", "g]", vim.diagnostic.goto_next, "Go to next diagnostic issue")
+        map("n", "g[", vim.diagnostic.goto_prev, "Go to previous diagnostic issue")
+      end
+
+      lsp[server].setup(opt)
+    end
+
+    if type(mason_lspconfig.setup_handlers) == "function" then
+      mason_lspconfig.setup_handlers({
+        function(server)
+          setup_server(server)
+        end,
+      })
+    else
+      -- Fallback for environments where setup_handlers is unavailable
+      for _, server in ipairs(M.servers) do
+        setup_server(server)
+      end
+    end
 
     -- Diagnostic settings
     vim.lsp.handlers["textDocument/publishDiagnostics"] =
