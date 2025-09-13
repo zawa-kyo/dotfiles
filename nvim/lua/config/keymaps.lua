690d996cfd251052fd2698b8ac2a78826c37e052
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-09-14 01:40:50 +0900

    feat: Update close_window mapping description

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -224,4 +224,4 @@ local function close_window()
   end
 end
 
-keymap("n", "<Esc>", close_window, opts("Close hover/clear search"))
+keymap("n", "<Esc>", close_window, opts("Close hover or clear search highlight"))
