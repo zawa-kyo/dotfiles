7783e103798996092834d46434999335bea69ebd
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-09-27 21:29:12 +0900

    fix: Switch from deprecated vim.lsp.diagnostic to vim.diagnostic.config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -155,8 +155,12 @@ table.insert(M, {
     end
 
     -- Diagnostic settings
-    vim.lsp.handlers["textDocument/publishDiagnostics"] =
-      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
+    vim.diagnostic.config({
+      virtual_text = false,
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+    })
   end,
 })
 
