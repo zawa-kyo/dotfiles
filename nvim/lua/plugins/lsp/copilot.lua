c363fafc0c23903107f50c29b2a8da1c5a9d1d2f
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-17 21:42:12 +0900

    refactor: Split LSP plugin specs into focused modules

---
+++ nvim/lua/plugins/lsp/copilot.lua
@@ -0,0 +1,33 @@
+-- Copilot support (both core plugin and cmp source bridge)
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    lazy = true,
+    event = "InsertEnter",
+    cond = not vim.g.vscode,
+    config = function()
+      require("copilot").setup({
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          keymap = {
+            accept = "<C-a>",
+            accept_word = false,
+            accept_line = false,
+          },
+        },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    lazy = true,
+    event = "InsertEnter",
+    cond = not vim.g.vscode,
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
