2edc163e32f15e9ebb2a085c53b8ab9738b3c428
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-17 21:05:08 +0900

    feat: Move window navigation keys to shifted leader

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -44,15 +44,15 @@ vim.g.maplocalleader = " "
 -- Note: Enabling ‘silent’ may cause rendering delay
 keymap("n", "<leader><leader>", ":", opts("Show command-line mode", true, false, nil))
 
--- Window navigation (Ctrl+h/j/k/l or <leader>h/j/k/l)
+-- Window navigation (Ctrl+h/j/k/l or <leader>H/J/K/J)
 keymap("n", "<C-h>", "<C-w>h", opts("Go to left window"))
 keymap("n", "<C-j>", "<C-w>j", opts("Go to bottom window"))
 keymap("n", "<C-k>", "<C-w>k", opts("Go to top window"))
 keymap("n", "<C-l>", "<C-w>l", opts("Go to right window"))
-keymap("n", "<leader>h", "<C-w>h", opts("Go to the left window"))
-keymap("n", "<leader>j", "<C-w>j", opts("Go to the bottom window"))
-keymap("n", "<leader>k", "<C-w>k", opts("Go to the top window"))
-keymap("n", "<leader>l", "<C-w>l", opts("Go to the right window"))
+keymap("n", "<leader>H", "<C-w>h", opts("Go to the left window"))
+keymap("n", "<leader>J", "<C-w>j", opts("Go to the bottom window"))
+keymap("n", "<leader>K", "<C-w>k", opts("Go to the top window"))
+keymap("n", "<leader>L", "<C-w>l", opts("Go to the right window"))
 
 -- Window operations (<leader>w…)
 keymap("n", "<leader>ws", ":split<CR><C-w>w", opts("Split window horizontally"))
