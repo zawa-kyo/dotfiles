dc8e94db3570e2a77e9be4718cc4393f933711ba
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-20 23:15:57 +0900

    feat: Load Lua snippets and add markdown caution snippet

--- nvim/lua/plugins/lua-snip.lua
+++ nvim/lua/plugins/lua-snip.lua
@@ -7,5 +7,8 @@ return {
 
   config = function()
     require("luasnip.loaders.from_vscode").lazy_load()
+    require("luasnip.loaders.from_lua").lazy_load({
+      paths = { vim.fn.stdpath("config") .. "/lua/snippets" },
+    })
   end,
 }
