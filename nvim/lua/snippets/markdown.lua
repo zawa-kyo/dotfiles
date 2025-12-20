fa37842616728ae73f0a15a1479eb8c60be96a61
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-20 23:56:40 +0900

    feat: Remove markdown callout snippets and add TODO placeholder

--- nvim/lua/snippets/markdown.lua
+++ nvim/lua/snippets/markdown.lua
@@ -4,24 +4,5 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
-  s("note", {
-    t({ ">[!NOTE]", ">" }),
-    i(1),
-  }),
-  s("tip", {
-    t({ ">[!TIP]", ">" }),
-    i(1),
-  }),
-  s("important", {
-    t({ ">[!IMPORTANT]", ">" }),
-    i(1),
-  }),
-  s("warning", {
-    t({ ">[!WARNING]", ">" }),
-    i(1),
-  }),
-  s("caution", {
-    t({ ">[!CAUTION]", ">" }),
-    i(1),
-  }),
+  -- TODO: Add more snippets here later as ideas come up.
 }
