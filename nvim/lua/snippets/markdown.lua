c1e32ffd432fb7f77111532cc5e43fd369537a01
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-20 23:19:41 +0900

    feat: Add common Markdown callout snippets

--- nvim/lua/snippets/markdown.lua
+++ nvim/lua/snippets/markdown.lua
@@ -4,6 +4,22 @@ local t = ls.text_node
 local i = ls.insert_node
 
 return {
+  s("note", {
+    t({ ">[!NOTE]", ">" }),
+    i(1),
+  }),
+  s("tip", {
+    t({ ">[!TIP]", ">" }),
+    i(1),
+  }),
+  s("important", {
+    t({ ">[!IMPORTANT]", ">" }),
+    i(1),
+  }),
+  s("warning", {
+    t({ ">[!WARNING]", ">" }),
+    i(1),
+  }),
   s("caution", {
     t({ ">[!CAUTION]", ">" }),
     i(1),
