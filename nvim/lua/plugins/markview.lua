5eb502a2a489e38d3fb2b1954fb55385e453c78e
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-26 23:24:00 +0900

    feat: Enhance markview presets and preview config

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -1,12 +1,26 @@
 return {
   "OXY2DEV/markview.nvim",
+
   lazy = false,
+  cond = not vim.g.vscode,
 
   config = function()
     local presets = require("markview.presets").headings
+
     require("markview").setup({
       markdown = {
         headings = presets.glow,
+        horizontal_rules = presets.thin,
+        tables = presets.rounded,
+      },
+
+      preview = {
+        enable = true,
+        enable_hybrid_mode = true,
+        modes = { "n", "i" },
+        hybrid_modes = { "n", "i" },
+        linewise_hybrid_mode = true,
+        edit_range = { 0, 0 },
       },
     })
   end,
