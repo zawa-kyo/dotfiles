64ebbfae74f2de04af917a4d92714a41605e6219
Kyohei Ozawa <zawazawakyo@gmail.com>
2025-12-28 15:08:55 +0900

    wip: Replace hop.nvim with flash.nvim

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,79 +1,65 @@
 return {
-  "smoka7/hop.nvim",
-
-  lazy = true,
-  event = {
-    "BufRead",
-    "BufNewFile",
-  },
-
-  opts = {
-    multi_windows = true,
-  },
-
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
   keys = {
-    -- Jump to a word
     {
       "m",
-      "<cmd>HopWord<CR>",
       mode = "n",
-      desc = "Hop Word",
+      desc = "Flash Word (HopWord-like)",
+      function()
+        require("flash").jump({
+          search = { multi_window = false },
+          matcher = function(win)
+            local matches = {}
+            local buf = vim.api.nvim_win_get_buf(win)
+            local l0 = vim.fn.line("w0", win)
+            local l1 = vim.fn.line("w$", win)
+
+            for lnum = l0, l1 do
+              local line = vim.api.nvim_buf_get_lines(buf, lnum - 1, lnum, false)[1] or ""
+              local col = 0
+              while true do
+                local s, e = line:find("[%w_]+", col + 1)
+                if not s or not e then
+                  break
+                end
+                table.insert(matches, {
+                  win = win,
+                  pos = { lnum, s - 1 },
+                  end_pos = { lnum, e - 1 },
+                })
+                col = e
+              end
+            end
+            return matches
+          end,
+        })
+      end,
     },
 
-    -- Jump to a line
     {
       "M",
-      "<cmd>HopLine<CR>",
       mode = "n",
-      desc = "Hop Line",
-    },
-
-    -- Jump to a character in the current line (after the cursor)
-    {
-      "f",
-      "<cmd>HopChar1CurrentLineAC<CR>",
-      mode = { "n", "v", "o" },
-      desc = "Hop Char (after cursor)",
-    },
-
-    -- Jump to a character in the current line (before the cursor)
-    {
-      "F",
-      "<cmd>HopChar1CurrentLineBC<CR>",
-      mode = { "n", "v", "o" },
-      desc = "Hop Char (before cursor)",
-    },
-
-    -- Jump to just before a character in the current line (after the cursor)
-    {
-      "s",
+      desc = "Flash Line (HopLine-like)",
       function()
-        local hop = require("hop")
-        local hint = require("hop.hint")
-        hop.hint_char1({
-          direction = hint.HintDirection.AFTER_CURSOR,
-          current_line_only = true,
-          hint_offset = -1,
+        require("flash").jump({
+          search = { multi_window = false },
+          matcher = function(win)
+            local matches = {}
+            local l0 = vim.fn.line("w0", win)
+            local l1 = vim.fn.line("w$", win)
+            for lnum = l0, l1 do
+              table.insert(matches, {
+                win = win,
+                pos = { lnum, 0 },
+                end_pos = { lnum, 0 },
+              })
+            end
+            return matches
+          end,
         })
       end,
-      mode = { "n", "v", "o" },
-      desc = "Hop before char (after cursor)",
-    },
-
-    -- Jump to just after a character in the current line (before the cursor)
-    {
-      "S",
-      function()
-        local hop = require("hop")
-        local hint = require("hop.hint")
-        hop.hint_char1({
-          direction = hint.HintDirection.BEFORE_CURSOR,
-          current_line_only = true,
-          hint_offset = 1,
-        })
-      end,
-      mode = { "n", "v", "o" },
-      desc = "Hop after char (before cursor)",
     },
   },
 }
