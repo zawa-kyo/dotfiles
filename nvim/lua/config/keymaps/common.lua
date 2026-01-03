c9f79adfa127a4bcc8154f0e59448e0d0621b598
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-03 22:04:32 +0900

    refactor: Split keymaps into modules

---
+++ nvim/lua/config/keymaps/common.lua
@@ -0,0 +1,80 @@
+local utils = require("config.utils")
+local opts = utils.getOpts
+local keymap = utils.getKeymap
+
+--------------------
+-- All Modes
+--------------------
+
+local all_modes = { "n", "i", "v", "x", "o", "t", "c", "s" }
+
+-- Disable arrow keys to encourage hjkl
+keymap(all_modes, "<Up>", "<Nop>", opts("Nop"))
+keymap(all_modes, "<Down>", "<Nop>", opts("Nop"))
+keymap(all_modes, "<Left>", "<Nop>", opts("Nop"))
+keymap(all_modes, "<Right>", "<Nop>", opts("Nop"))
+
+--------------------
+-- Normal Mode
+--------------------
+
+-- Better transition to command-line mode
+-- Note: Enabling ‘silent’ may cause rendering delay
+keymap("n", "<leader><leader>", ":", opts("Show command-line mode", true, false, nil))
+
+-- Window navigation (Ctrl+h/j/k/l or <leader>H/J/K/L)
+keymap("n", "<C-h>", "<C-w>h", opts("Go to left window"))
+keymap("n", "<C-j>", "<C-w>j", opts("Go to bottom window"))
+keymap("n", "<C-k>", "<C-w>k", opts("Go to top window"))
+keymap("n", "<C-l>", "<C-w>l", opts("Go to right window"))
+keymap("n", "<leader>H", "<C-w>h", opts("Go to the left window"))
+keymap("n", "<leader>J", "<C-w>j", opts("Go to the bottom window"))
+keymap("n", "<leader>K", "<C-w>k", opts("Go to the top window"))
+keymap("n", "<leader>L", "<C-w>l", opts("Go to the right window"))
+
+-- Window operations (<leader>w…)
+keymap("n", "<leader>ws", ":split<CR><C-w>w", opts("Split window horizontally"))
+keymap("n", "<leader>wv", ":vsplit<CR><C-w>w", opts("Split window vertically"))
+keymap("n", "<leader>w=", "<C-w>=", opts("Equalize window sizes"))
+keymap("n", "<leader>wq", "<C-w>q", opts("Close the current window"))
+
+-- Resize (use shifted H/J/K/L to imply “bigger action”)
+keymap("n", "<leader>wH", "5<C-w><", opts("Decrease window width"))
+keymap("n", "<leader>wL", "5<C-w>>", opts("Increase window width"))
+keymap("n", "<leader>wJ", "5<C-w>-", opts("Decrease window height"))
+keymap("n", "<leader>wK", "5<C-w>+", opts("Increase window height"))
+
+-- Buffer operations (<leader>b…)
+keymap("n", "<leader>bl", "<Cmd>bnext<CR>", opts("Go to next buffer"))
+keymap("n", "<leader>bh", "<Cmd>bprevious<CR>", opts("Go to previous buffer"))
+keymap("n", "<leader>bb", "<Cmd>ls<CR>", opts("List buffers"))
+keymap("n", "<leader>bd", "<Cmd>bdelete<CR>", opts("Delete buffer"))
+
+-- Tab operations (<leader>t…)
+keymap("n", "<leader>tn", ":tabedit", opts("Open new tab (enter filename)"))
+keymap("n", "<leader>tl", "gt", opts("Go to next tab"))
+keymap("n", "<leader>th", "gT", opts("Go to previous tab"))
+keymap("n", "<leader>tq", "<Cmd>tabclose<CR>", opts("Close tab"))
+
+-- Disable default substitute to free up "s" prefix
+keymap("n", "s", "<Nop>", opts("Disable substitute"))
+
+-- Wrapped lines: move by screen line
+keymap("n", "j", "gj", opts("Move down wrapped lines"))
+keymap("n", "k", "gk", opts("Move up wrapped lines"))
+
+-- Jump history (keep gp/gP)
+keymap("n", "gp", "<C-o>", opts("Jump to previous location"))
+keymap("n", "gP", "<C-i>", opts("Jump to next location"))
+keymap("n", "gn", "<C-i>", opts("Jump to next location"))
+keymap("n", "gN", "<C-o>", opts("Jump to previous location"))
+
+-- Match pairs
+keymap("n", "M", "%", opts("Jump to the matching pair"))
+
+-- Clipboard
+keymap({ "n", "v" }, "<leader>y", '"+y', opts("Copy to clipboard"))
+
+-- Look around
+keymap("n", "zk", "zb", opts("Look up"))
+keymap("n", "zj", "zt", opts("Look down"))
---
+++ nvim/lua/config/keymaps/editing.lua
@@ -0,0 +1,83 @@
+local utils = require("config.utils")
+local opts = utils.getOpts
+local keymap = utils.getKeymap
+
+--------------------
+-- Normal Mode
+--------------------
+
+-- Select all
+keymap("n", "<leader>a", "ggVG", opts("Select all"))
+keymap("v", ",", "<Esc>ggVG", opts("Select all"))
+
+-- Do not yank with x
+keymap("n", "x", '"_x', opts("Do not yank with x"))
+keymap("n", "<BS>", '"_x', opts("Do not yank with backspace"))
+
+-- Create new lines without reaching for o/O
+keymap("n", "<CR>", "o", opts("Insert new line below"))
+keymap("n", "<S-CR>", "O", opts("Insert new line above"))
+
+-- Redo
+keymap("n", "U", "<C-r>", opts("Redo"))
+
+-- Move current line up/down
+keymap("n", "<leader>K", "<Cmd>move -2<CR>==", opts("Move current line up"))
+keymap("n", "<leader>J", "<Cmd>move +1<CR>==", opts("Move current line down"))
+
+-- Indent after pasting
+keymap("n", "p", "]p`]", opts("Indent after pasting"))
+keymap("n", "P", "]P`]", opts("Indent after pasting"))
+
+-- Auto-indent when starting edit on an empty line
+keymap("n", "i", function()
+  return vim.fn.getline(".") == "" and '"_cc' or "i"
+end, opts("Indent when starting editing on an empty line", nil, nil, true))
+keymap("n", "A", function()
+  return vim.fn.getline(".") == "" and '"_cc' or "A"
+end, opts("Indent when starting editing on an empty line", nil, nil, true))
+
+--------------------
+-- Insert Mode
+--------------------
+
+-- Auto space after comma
+keymap("i", ",", ",<Space>", opts("Insert a space after a comma"))
+
+-- Remap jj to Esc
+keymap("i", "jj", "<Esc>", opts("Escape", true, false, nil))
+
+--------------------
+-- Visual Mode
+--------------------
+
+-- Indentation
+keymap("v", "<", "<gv", opts("Add indentation"))
+keymap("v", ">", ">gv", opts("Reduce indentation"))
+
+-- Align Visual behavior
+keymap("v", "v", "<Esc>V", opts("Select the whole line"))
+keymap("n", "V", "v$", opts("Select until the end of the line"))
+
+-- Preserve cursor on yank
+keymap("x", "y", "mzy`z", opts("Yank the selected text"))
+
+-- Delete words with backspace
+keymap({ "v", "x" }, "<BS>", "_d", opts("Delete selection with backspace"))
+
+-- Move selected lines
+keymap("x", "K", ":move '<-2<CR>gv=gv", opts("Move selected lines up"))
+keymap("x", "J", ":move '>+1<CR>gv=gv", opts("Move selected lines down"))
+
+-- Text objects: avoid leading/trailing spaces when appending
+for _, quote in ipairs({ '"', "'", "`" }) do
+  keymap({ "x", "o" }, "a" .. quote, "2i" .. quote, opts("Append without leading/trailing spaces"))
+end
+
+--------------------
+-- Text Object
+--------------------
+
+-- Between spaces
+keymap("o", "i<space>", "iW", opts("Select words between spaces"))
+keymap("x", "i<space>", "iW", opts("Select words between spaces"))
