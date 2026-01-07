1080a1768417f3dc2c8291f8c61bcb8c6aaed28a
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-07 11:23:02 +0900

    fix: Lazy-load fzf actions

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,7 +1,11 @@
 local utils = require("config.utils")
-local fzf_actions = require("fzf-lua.actions")
+local fzf_actions = nil
 
 local function toggle_visibility_actions()
+  if not fzf_actions then
+    fzf_actions = require("fzf-lua.actions")
+  end
+
   return {
     ["ctrl-u"] = { fzf_actions.toggle_hidden, { desc = "Toggle hidden" } },
     ["ctrl-o"] = { fzf_actions.toggle_ignore, { desc = "Toggle ignore" } },
