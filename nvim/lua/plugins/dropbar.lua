2e3d27d70fc68129ee98dcb8c80ed2379fd9c6b6
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-11 21:26:24 +0900

    feat: Add dropbar toggle and lazy-load setup

--- nvim/lua/plugins/dropbar.lua
+++ nvim/lua/plugins/dropbar.lua
@@ -1,6 +1,52 @@
 return {
   "Bekaboo/dropbar.nvim",
 
-  event = "VeryLazy",
   cond = not vim.g.vscode,
+  event = { "BufReadPre", "BufNewFile" },
+  lazy = true,
+
+  keys = {
+    { "tb", "<Cmd>DropbarToggle<CR>", desc = "Toggle dropbar" },
+  },
+
+  -- TODO: Review and refine dropbar settings later.
+  config = function()
+    local dropbar = require("dropbar")
+    local configs = require("dropbar.configs")
+    local utils = require("dropbar.utils")
+    local default_enable = configs.opts.bar.enable
+
+    local function is_enabled(win)
+      local ok, value = pcall(vim.api.nvim_win_get_var, win, "dropbar_enabled")
+      if not ok then
+        return false
+      end
+      return value ~= false
+    end
+
+    dropbar.setup({
+      bar = {
+        enable = function(buf, win, info)
+          if not is_enabled(win) then
+            return false
+          end
+          return configs.eval(default_enable, buf, win, info)
+        end,
+      },
+    })
+
+    vim.api.nvim_create_user_command("DropbarToggle", function()
+      local win = vim.api.nvim_get_current_win()
+      local enabled = not is_enabled(win)
+      vim.api.nvim_win_set_var(win, "dropbar_enabled", enabled)
+      if enabled then
+        utils.bar.attach(vim.api.nvim_win_get_buf(win), win)
+        utils.bar.exec("update", { win = win })
+        return
+      end
+
+      utils.bar.exec("del", { win = win })
+      vim.wo[win].winbar = ""
+    end, {})
+  end,
 }
