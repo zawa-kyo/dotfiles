b098e146bd709da3bf57d893760b53a10507ce96
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-15 15:44:52 +0900

    fix: Avoid table.unpack for cursor position

--- nvim/lua/plugins/lsp/completion.lua
+++ nvim/lua/plugins/lsp/completion.lua
@@ -22,7 +22,8 @@ return {
         if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
           return false
         end
-        local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local line, col = cursor[1], cursor[2]
         local text = vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]
         return col ~= 0 and text:match("^%s*$") == nil
       end
