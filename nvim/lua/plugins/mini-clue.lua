2450204bc02fac8dea82babb8afa51a252c14f43
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-15 15:20:22 +0900

    feat: Replace which-key with mini.clue

---
+++ nvim/lua/plugins/mini-clue.lua
@@ -0,0 +1,42 @@
+return {
+  "echasnovski/mini.clue",
+  cond = not vim.g.vscode,
+  event = "VeryLazy",
+  config = function()
+    local miniclue = require("mini.clue")
+    local gen_clues = miniclue.gen_clues
+
+    local triggers = {
+      { mode = { "n", "x" }, keys = "<Leader>" },
+      { mode = "n", keys = "[" },
+      { mode = "n", keys = "]" },
+      { mode = "i", keys = "<C-x>" },
+      { mode = { "n", "x" }, keys = "g" },
+      { mode = { "n", "x" }, keys = "'" },
+      { mode = { "n", "x" }, keys = "`" },
+      { mode = { "n", "x" }, keys = '"' },
+      { mode = { "i", "c" }, keys = "<C-r>" },
+      { mode = "n", keys = "<C-w>" },
+      { mode = { "n", "x" }, keys = "z" },
+      { mode = { "n", "x" }, keys = "s" },
+      { mode = { "x", "o" }, keys = "i" },
+      { mode = { "x", "o" }, keys = "a" },
+      { mode = "n", keys = "m" },
+    }
+
+    local clues = {
+      gen_clues.square_brackets(),
+      gen_clues.builtin_completion(),
+      gen_clues.g(),
+      gen_clues.marks(),
+      gen_clues.registers({ show_contents = true }),
+      gen_clues.windows({ submode_resize = true, submode_move = true }),
+      gen_clues.z(),
+    }
+
+    miniclue.setup({
+      triggers = triggers,
+      clues = clues,
+    })
+  end,
+}
