765ef0e02534a725cd1303649b14b8c830ff89b5
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-15 15:20:50 +0900

    refactor: Move treesitter textobjects into mini.ai

--- nvim/lua/plugins/mini-ai.lua
+++ nvim/lua/plugins/mini-ai.lua
@@ -4,17 +4,22 @@ return {
   event = "VeryLazy",
   dependencies = { "echasnovski/mini.extra" },
   config = function()
+    local ai = require("mini.ai")
     local gen_ai_spec = require("mini.extra").gen_ai_spec
+    local treesitter = ai.gen_spec.treesitter
 
-    require("mini.ai").setup({
-      custom_textobjects = {
-        B = gen_ai_spec.buffer(),
-        D = gen_ai_spec.diagnostic(),
-        I = gen_ai_spec.indent(),
-        L = gen_ai_spec.line(),
-        N = gen_ai_spec.number(),
-        J = { { "()%d%d%d%d%-%d%d%-%d%d()", "()%d%d%d%d%/%d%d%/%d%d()" } },
-      },
+    local custom_textobjects = {
+      c = treesitter({ a = "@class.outer", i = "@class.inner" }),
+      f = treesitter({ a = "@function.outer", i = "@function.inner" }),
+      B = gen_ai_spec.buffer(),
+      D = gen_ai_spec.diagnostic(),
+      I = gen_ai_spec.indent(),
+      L = gen_ai_spec.line(),
+      N = gen_ai_spec.number(),
+      J = { { "()%d%d%d%d%-%d%d%-%d%d()", "()%d%d%d%d%/%d%d%/%d%d()" } },
+    }
+    ai.setup({
+      custom_textobjects = custom_textobjects,
     })
   end,
 }
