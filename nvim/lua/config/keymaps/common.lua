50edd1ed9ce4cb88aec3236cd32807f8b32333b2
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-16 21:57:01 +0900

    style: Clarify s/r keymap comment

--- nvim/lua/config/keymaps/common.lua
+++ nvim/lua/config/keymaps/common.lua
@@ -60,11 +60,9 @@ keymap("n", "<leader>tl", "gt", opts("Go to next tab"))
 keymap("n", "<leader>th", "gT", opts("Go to previous tab"))
 keymap("n", "<leader>tq", "<Cmd>tabclose<CR>", opts("Close tab"))
 
--- Disable default substitute to free up "s/S" prefix
+-- Disable default substitute to free up "s/S", "r/R" prefix
 keymap("n", "s", "<Nop>", opts("Disable substitute"))
 keymap("n", "S", "<Nop>", opts("Disable substitute line"))
-
--- Disable default replace to free up "r/R" prefix
 keymap("n", "r", "<Nop>", opts("Disable replace"))
 keymap("n", "R", "<Nop>", opts("Disable replace line"))
 
