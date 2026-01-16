97d3ed9f942760b3802e74709faba676ecd1221c
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-16 13:54:00 +0900

    feat: Show active LSP clients in lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,6 +9,33 @@ return {
   },
 
   config = function()
+    local ignore_lsp_list = {
+      ["null-ls"] = true,
+      ["null_ls"] = true,
+    }
+
+    local function lsp_clients()
+      local clients = {}
+      if vim.lsp.get_clients then
+        clients = vim.lsp.get_clients({ bufnr = 0 })
+      else
+        clients = vim.lsp.get_active_clients({ bufnr = 0 })
+      end
+
+      local names = {}
+      for _, client in ipairs(clients) do
+        if not ignore_lsp_list[client.name] then
+          table.insert(names, client.name)
+        end
+      end
+
+      if #names == 0 then
+        return "No LSP"
+      end
+
+      return table.concat(names, ", ")
+    end
+
     local function setup_lualine()
       require("lualine").setup({
         options = {
@@ -23,7 +50,9 @@ return {
           lualine_b = { "filename" },
           lualine_c = { "diff" },
           lualine_x = { "diagnostics" },
-          lualine_y = { "lsp_status" },
+          lualine_y = {
+            { lsp_clients, icon = " " },
+          },
           lualine_z = { "filetype" },
         },
 
