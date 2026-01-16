4800bc3f2662433241e92a34662ca3c48a79302d
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-16 13:54:35 +0900

    feat: Simplify lualine inactive statusline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -60,7 +60,7 @@ return {
           lualine_a = {},
           lualine_b = {},
           lualine_c = { "filename" },
-          lualine_x = { "location" },
+          lualine_x = {},
           lualine_y = {},
           lualine_z = {},
         },
