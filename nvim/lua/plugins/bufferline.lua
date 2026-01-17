d9fc545fb668c902a8f79ad5440395e112f1ca46
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-17 20:29:56 +0900

    feat: Configure bufferline options

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,16 @@ return {
 
   lazy = false,
   cond = not vim.g.vscode,
-
   dependencies = "DaikyXendo/nvim-material-icon",
+
+  -- TODO: Customize the bufferline appearance.
+  opts = {
+    options = {
+      mode = "buffers",
+      always_show_bufferline = true,
+      show_close_icon = false,
+      show_buffer_close_icons = false,
+      show_tab_indicators = false,
+    },
+  },
 }
