fbd66754ec1762676c6a7dd3fceccb8914a0557f
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-18 17:39:31 +0900

    feat: Add flash search bindings

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -21,14 +21,21 @@ return {
   },
 
   keys = {
-    -- TODO: Consider mapping these frequently used actions to single keys.
     {
-      "gw", -- go word
-      mode = { "n", "x", "o" },
+      "/",
+      mode = "n",
       function()
         require("flash").jump()
       end,
-      desc = "Flash jump",
+      desc = "Flash search",
+    },
+    {
+      "?",
+      mode = "n",
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash search with treesitter",
     },
     {
       "gl", -- go line
