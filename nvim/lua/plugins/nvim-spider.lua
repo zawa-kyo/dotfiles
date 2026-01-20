25be410c8c953954e65c85b003a757978e52b22b
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-20 10:48:48 +0900

    feat: Bind w/e/b/ge to nvim-spider motions

--- nvim/lua/plugins/nvim-spider.lua
+++ nvim/lua/plugins/nvim-spider.lua
@@ -1,9 +1,15 @@
 return {
-    "chrisgrieser/nvim-spider",
+  "chrisgrieser/nvim-spider",
 
-    lazy = true,
-    event = {
-        "BufRead",
-        "BufNewFile",
-    },
+  event = {
+    "BufRead",
+    "BufNewFile",
+  },
+
+  keys = {
+    { "w",  "<cmd>lua require('spider').motion('w')<CR>",  mode = { "n", "o", "x" } },
+    { "e",  "<cmd>lua require('spider').motion('e')<CR>",  mode = { "n", "o", "x" } },
+    { "b",  "<cmd>lua require('spider').motion('b')<CR>",  mode = { "n", "o", "x" } },
+    { "ge", "<cmd>lua require('spider').motion('ge')<CR>", mode = { "n", "o", "x" } },
+  },
 }
