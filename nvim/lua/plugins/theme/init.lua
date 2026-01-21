271dc5591e020339ff043f961b91476d5afe0d3c
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-21 13:04:23 +0900

    fix: Reapply snacks diff highlights on ColorScheme

--- nvim/lua/plugins/theme/init.lua
+++ nvim/lua/plugins/theme/init.lua
@@ -1,3 +1,43 @@
+local function set_snacks_diff_hl()
+  local function get_hl(name)
+    local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = name })
+    if ok then
+      return hl
+    end
+    return {}
+  end
+
+  local function strip_bg(hl)
+    return {
+      fg = hl.fg,
+      bg = nil,
+      bold = hl.bold,
+      italic = hl.italic,
+      underline = hl.underline,
+    }
+  end
+
+  local add = get_hl("DiffAdd")
+  local del = get_hl("DiffDelete")
+  local change = get_hl("DiffChange")
+  local comment = get_hl("Comment")
+  local warn = get_hl("DiagnosticVirtualTextWarn")
+
+  vim.api.nvim_set_hl(0, "SnacksDiffHeader", strip_bg(comment))
+  vim.api.nvim_set_hl(0, "SnacksDiffAdd", strip_bg(add))
+  vim.api.nvim_set_hl(0, "SnacksDiffDelete", strip_bg(del))
+  vim.api.nvim_set_hl(0, "SnacksDiffContext", strip_bg(change))
+  vim.api.nvim_set_hl(0, "SnacksDiffConflict", strip_bg(warn))
+  vim.api.nvim_set_hl(0, "SnacksDiffAddLineNr", strip_bg(add))
+  vim.api.nvim_set_hl(0, "SnacksDiffDeleteLineNr", strip_bg(del))
+  vim.api.nvim_set_hl(0, "SnacksDiffContextLineNr", strip_bg(change))
+  vim.api.nvim_set_hl(0, "SnacksDiffConflictLineNr", strip_bg(warn))
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = set_snacks_diff_hl,
+})
+
 return {
   require("plugins.theme.nordfox"),
   require("plugins.theme.nordic"),
