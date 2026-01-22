ab299331e7269eaf2c70c232523c5c2f08618b14
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-22 16:33:38 +0900

    refactor: Move built-in plugin disables to bootstrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,8 @@
 -- Speed up Lua module loading (Neovim 0.9+)
 if vim.loader then vim.loader.enable() end
 
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_netrw = 1
-vim.g.Loaded_netrwPlugin = 1
+-- Disable some built-in plugins
+require("config.bootstrap")
 
+-- Load the main configuration
 require("config")
---
+++ nvim/lua/config/bootstrap.lua
@@ -0,0 +1,5 @@
+-- Disable built-in plugins we do not use.
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrw = 1
+vim.g.Loaded_netrwPlugin = 1
