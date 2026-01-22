56465cad9029bfea4c72bbef988c1b95749830a6
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-22 16:40:16 +0900

    fix: Move built-in plugin disables into lazy config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,5 @@
 -- Speed up Lua module loading (Neovim 0.9+)
 if vim.loader then vim.loader.enable() end
 
--- Disable some built-in plugins
-require("config.bootstrap")
-
 -- Load the main configuration
 require("config")
