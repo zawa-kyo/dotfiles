c81339d14150b8f4bebe9439688be77fda1ecf94
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-22 16:31:57 +0900

    feat: Disable built-in matchit/matchparen/netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
 -- Speed up Lua module loading (Neovim 0.9+)
 if vim.loader then vim.loader.enable() end
 
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrw = 1
+vim.g.Loaded_netrwPlugin = 1
+
 require("config")
