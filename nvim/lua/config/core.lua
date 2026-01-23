c6da43046dea0b4f56ddcd46cfb11c10e3b5ff42
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-23 20:18:45 +0900

    fix: Drop explicit filetype plugin/indent init

--- nvim/lua/config/core.lua
+++ nvim/lua/config/core.lua
@@ -1,8 +1,4 @@
 vim.scriptencoding = "utf-8"
 
--- Ensure filetype detection is enabled early
-vim.g.do_filetype_lua = 1
-vim.cmd("filetype plugin indent on")
-
 -- Fallback syntax highlighting (in case Treesitter/theme misses some groups)
 vim.cmd("syntax enable")
