0e2dbd8d0967ea5a3b5b343b6acf31e2263eb5ae
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-01-24 10:40:13 +0900

    feat: Refine grug-far keymaps

--- nvim/lua/plugins/grug-far.lua
+++ nvim/lua/plugins/grug-far.lua
@@ -5,9 +5,24 @@ return {
 
   keys = {
     {
-      "sG",
-      "<cmd>GrugFar<CR>",
-      desc = "Search and replace (GrugFar)",
+      "rr",
+      function()
+        require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+      end,
+      desc = "Search and replace in current file",
+    },
+    {
+      "rR",
+      function()
+        require("grug-far").open()
+      end,
+      desc = "Search and replace in workspace",
+    },
+    {
+      "r",
+      ":'<,'>GrugFarWithin<CR>",
+      mode = "v",
+      desc = "Search and replace within selection",
     },
   },
 }
