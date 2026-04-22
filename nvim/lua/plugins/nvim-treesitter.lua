e407497987360c09880841017fca2dca69fbe91b
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-04-22 23:37:00 +0900

    fix: Use built-in vim parser for treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -18,4 +18,16 @@ return {
     "nvim-treesitter/nvim-treesitter-context",
   },
   build = ":TSUpdate",
+  config = function()
+    local function use_builtin_parser(lang)
+      local paths = vim.api.nvim_get_runtime_file(("parser/%s.so"):format(lang), true)
+      local builtin = paths[#paths]
+      if builtin and builtin:match("/lib/nvim/parser/") then
+        vim.treesitter.language.add(lang, { path = builtin })
+      end
+    end
+
+    -- nvim-treesitter main currently ships a vim parser older than Neovim 0.12's bundled one.
+    use_builtin_parser("vim")
+  end,
 }
