03f25514446c9b0e53947c28c40af71889db5ad0
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-04-25 11:22:07 +0900

    fix: Drop builtin vim parser override

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -17,13 +17,5 @@ return {
   },
   config = function(_, opts)
     require("nvim-treesitter").setup(opts)
-
-    -- Prefer Neovim's bundled vim parser until nvim-treesitter ships a compatible one.
-    for _, path in ipairs(vim.api.nvim_get_runtime_file("parser/vim.so", true)) do
-      if path:match("/lib/nvim/parser/") then
-        vim.treesitter.language.add("vim", { path = path })
-        break
-      end
-    end
   end,
 }
