a3b1c5db38f76f740c2bc6c972273628a1669c10
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-04-30 17:33:29 +0900

    fix: Split scripts into local and global groups

--- scripts/local/relink-dotfiles.sh
+++ scripts/local/relink-dotfiles.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-dotfiles_dir="$(cd "$script_dir/.." && pwd)"
-source "$script_dir/utils/dotfiles-links.sh"
-source "$script_dir/utils/log.sh"
+dotfiles_dir="$(cd "$script_dir/../.." && pwd)"
+source "$script_dir/../utils/dotfiles-links.sh"
+source "$script_dir/../utils/log.sh"
 
 relink() {
   local source_path="$1"
