4f8b3b9f0797ff9b9badaf166d837e1429681121
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-05-03 20:53:35 +0900

    refactor: Rename branch-ref helper

--- scripts/global/switch-branch.sh
+++ scripts/global/switch-branch.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 script_path="$(realpath "${BASH_SOURCE[0]}")"
 script_dir="$(cd "$(dirname "$script_path")" && pwd)"
 source "$script_dir/../utils/log.sh"
-source "$script_dir/../utils/branch-ref.sh"
+source "$script_dir/../utils/select-branch.sh"
 source "$script_dir/../utils/switch-branch.sh"
 
 run_switch_branch local "$@"
