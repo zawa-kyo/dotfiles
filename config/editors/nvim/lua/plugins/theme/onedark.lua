435a0a9e5dd34a60e81a73f538d74e1048c836f3
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-07-12 12:13:09 +0900

    fix: Defer onedark loading until it is selected

--- config/editors/nvim/lua/plugins/theme/onedark.lua
+++ config/editors/nvim/lua/plugins/theme/onedark.lua
@@ -1,13 +1,12 @@
 return {
   "navarasu/onedark.nvim",
 
-  priority = 1000,
+  lazy = true,
 
   config = function()
     require("onedark").setup({
       style = "cool",
       transparent = false,
     })
-    require("onedark").load()
   end,
 }
