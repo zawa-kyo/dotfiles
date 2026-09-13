9424bbf89201aaf8f946dec2bc9ce9e1ec602e2d
Kyohei Ozawa <zawazawakyo@gmail.com>
2026-09-13 14:51:09 +0900

    fix: Restore standard TODO picker integration

--- dotfiles/editors/nvim/lua/plugins/coding/todo-comments.lua
+++ dotfiles/editors/nvim/lua/plugins/coding/todo-comments.lua
@@ -1,35 +1,6 @@
--- Format TODO search results without Tree-sitter highlighting.
-local function format_todo_comment(item, picker)
-  local align = Snacks.picker.util.align
-  local _, _, keyword = require("todo-comments.highlight").match(item.text)
-  local result = {}
-
-  if keyword then
-    keyword = require("todo-comments.config").keywords[keyword] or keyword
-    local icon = vim.tbl_get(require("todo-comments.config").options.keywords, keyword, "icon") or ""
-    result[#result + 1] = { align(icon, 2), "TodoFg" .. keyword }
-    result[#result + 1] = { align(keyword, 6, { align = "center" }), "TodoBg" .. keyword }
-    result[#result + 1] = { " " }
-  end
-
-  vim.list_extend(result, Snacks.picker.format.filename(item, picker))
-
-  if item.line then
-    result[#result + 1] = { "  " }
-    result[#result + 1] = { item.line }
-  end
-
-  return result
-end
-
 -- Search the project TODO comments that need follow-up.
 local function search_todo_comments()
-  local snacks = require("snacks")
-  snacks.picker.sources.todo_comments = snacks.picker.sources.todo_comments or require("todo-comments.snacks").source
-  snacks.picker.pick("todo_comments", {
-    keywords = { "TODO", "FIX", "FIXME" },
-    format = format_todo_comment,
-  })
+  require("snacks").picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
 end
 
 return {
@@ -37,10 +8,7 @@ return {
 
   cond = not vim.g.vscode,
   event = { "BufRead", "BufNewFile" },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "folke/snacks.nvim",
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
 
   keys = {
     {
